5e7c57505cc698b68bac147327b522aa3f9ca602
Ray Gomez <rayvincent.gomez@gmail.com>
2015-10-07 18:02:40 +0800

    implemented exercise 3-6

---
+++ exercise3-6.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+__author__ = 'ragomez'
+
+
+class Car(object):
+
+    def __init__(self):
+        self.brand = ''
+        self.max_speed = 0
+
+    def set_brand(self, brand):
+        self.brand = brand
+
+    def set_max_speed(self, max_speed):
+        self.max_speed = max_speed
+
+    def print_data(self):
+        print('Car(brand:{},max speed:{})'.format(self.brand, self.max_speed))
+
+if __name__ == '__main__':
+
+    audi = Car()
+    audi.set_brand('Audi')
+    audi.set_max_speed(200)
+    audi.print_data()
